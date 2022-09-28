989d28489d32c8ed7501b365a0412c25a8476582
seanLimWeiRen <113512088+seanLimWeiRen@users.noreply.github.com>
2022-09-28 18:22:10 +0800

    Update main.py
    Ildob+KAmXMgam9lPyIgYSBkaXN0YW50IHZvaWNlIGFza3MuCgpJbnN0YW50bHkgZXZlcnlvbmUgbmVhcmJ5IGhlYXJzIHRoZSBzb3VuZCBvZiAxLDAwMHMgb2YgYnJpY2tzIHJhcGlkbHkgc2h1ZmZsaW5nIHRvd2FyZHMgaGlzIGxvY2F0aW9uLgoKVGhlIGVhcnRoIGl0c2VsZiBzZWVtZWQgdG8gY3J5IG91dCBpbiBhZ29ueSwgdW50aWwgZmluYWxseSB0aGUgZ3JvdW5kIGl0c2VsZiBzcGxpdCBvcGVuIGFuZCBhIGhvcnJpZmljIGNyZWF0dXJlIGNyYXdsZWQgZnJvbSB0aGUgZ3JvdW5kLCBjb3ZlcmVkIGluIG11Y3VzIGFuZCB0YXIuCgrigJ1Kb2UgTW9tbWHigKbigJ0gdGhlIGNyZWF0dXJlIHdoaXNwZXJlZC4KClRoZSBtYW4gY3JpZWQgb3V0IGluIHBhaW4gYXMgaGUgZGlzaW50ZWdyYXRlZCBpbnRvIGR1c3QsIGFuZCB0aGUgd2hvbGUgd29ybGQgZmVsbCBzaWxlbnQgaW4gZmVhci4KCiJJIGRpZCBhIGxpdHRsZSB0cm9sbGluZy4iIHRoZSB3cmV0Y2hlZCBjcmVhdHVyZSByZW1hcmtlZCBiZWZvcmUgYnVycm93aW5nIGJhY2sgaW50byB0aGUgZWFydGgu

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
-def main():
-  pass
+from microbit import *
+import music
 
-if __name__ == "__main__":
-    main()
+back = False
+front = False
+
+def alarm():
+    display.show(Image.HEART)
+    music.play(['f#'])
+
+
+
+while True:
+    z_strength = accelerometer.get_z()
+    
+    print("z " + str(z_strength)) # for debugging purposes
+    
+    if z_strength > 400:
+        back = True
+    else:
+        back = False
+    if z_strength < -400:
+        front = True
+    else:
+        front = False
+
+    if front == True:
+        alarm()
+    else:
+        if back == True:
+            alarm()
+        else:
+            display.clear()
+    
+    sleep(100)
+
+    
+
+
+
+
+
+
